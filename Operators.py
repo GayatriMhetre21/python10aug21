2a56d73646e6fbcc7c44bea12614bdc386fb631f
Gayatri Mhtre <88704532+GayatriMhetre21@users.noreply.github.com>
2021-08-13 11:53:04 -0700

    13aug21.prog

---
+++ Operators.py
@@ -0,0 +1,55 @@
+a = 20
+b = 10
+
+# Arithmetic Operators
+print("a + b = ",a+b)
+print("a - b = ",a-b)
+print("a * b = ",a*b)
+print("a / b = ",a/b)
+
+# Comparison Operators
+
+print("a < b",a<b)
+print("a > b",a>b)
+print("a <= b",a<=b)
+print("a >= b",a>=b)
+print("a == b",a==b)
+print("a != b",a!=b)
+
+# Logical Operators
+x = True
+y = False
+
+print("x and y is",x & y)
+print("x or y is",x or y)
+print("not y is",not y)
+
+# Identity Operators
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+
+print("x1 is not y1 ",x1 is not y1)
+print("x2 is y2",x2 is y2)
+
+# Membership Operators
+x = 'Hello world'
+y =  {1:'a',2:'b'}
+
+print("H in x",'H' in x)
+print("hello not in x",'hello' not in x)
+print("1 in y",1 in y)
+print("a in y",'a' in y)
+
+# bitwise Operators
+a = 60            
+b = 13       
+
+print("a & b =", a & b )
+print("a | b=" , a | b)    
+print("a ^ b=",a ^ b)        
+print(" ~a =", ~a)         
+print( "a << 2=",a << 2)      
+print("a >> 2=",a >> 2)      
+
