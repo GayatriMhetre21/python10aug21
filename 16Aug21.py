a1189c34be51ee0e4fb8c6e926dd6a3c8b8c9b31
Gayatri Mhtre <88704532+GayatriMhetre21@users.noreply.github.com>
2021-08-16 01:31:59 -0700

    Add files via upload

--- 16Aug21.py
+++ 16Aug21.py
@@ -1,15 +1,23 @@
 #Find ASCII Value of a character 'X' and 'x'
 c = 'x'
+print("\nThe ASCII value of '" + c + "' is", ord(c))
+c = 'X'
 print("The ASCII value of '" + c + "' is", ord(c))
+
+
+
 
 #Write Program to Compute Quotient and Remainder
 n=9
 s=4
 
 q=n/s
-print("Quotient is ",q)
+print("\nQuotient is ",q)
 r=n%s
 print("Remainder is",r)
+
+
+
 
 #Swap two numbers using temporary variable
 x = 5
@@ -19,41 +27,67 @@ temp = x        #created temporary variable
 x = y
 y = temp        #created temporary variable 
 
-print('The value of x after swapping: {}'.format(x))
+print('\nThe value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
+
+
 
 
 #Write Program to Check Whether a Number is Even or Odd - 88,37,1658
 
-num = int(input("Enter a number: "))
+num = int(input("\nEnter a number: "))
 if (num % 2) == 0:
-   print("{0} is Even".format(num))
+   print("\n{0} is Even".format(num))
 else:
-   print("{0} is Odd".format(num))
+   print("\n{0} is Odd".format(num))
+
 
 
 #Check whether an alphabet is vowel or consonant using if..else statement - a,b,e,o
-ch = input("Enter a character: ")
+ch = input("\nEnter a character: ")
 
 if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
  or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-    print(ch, "is a Vowel")
+    print(ch, "\nis a Vowel")
 else:
-    print(ch, "is a Consonant")
+    print(ch, "\nis a Consonant")
+
+
+
+#Write Programm to calculate GST i.e. 18% on base price 34900/-
+gst = ((18/100) * 34900)
+print("\nGST is ",)
+
+
 
 #Write programm to calculate monthly simple intrest and compound intrest(5%/Month) on amount - 161258/-
+p=161258
+t=1
+r=5
 
-    p=161258
-    t=30
-    r=5
-    print('The principal is', p)
-    print('The time period is', t)
-    print('The rate of interest is',r)
-      
-    si = (p * t * r)/100
-      
-    print('The Simple Interest is', si)
-    
+print('The principal is', p)
+print('The time period is', t)
+print('The rate of interest is',r)
+si = (p * t * r)/100
+print('\nThe Simple Interest is', si)
+
+
+
+#Write program to generate equated monthly instalments (EMI)(intrest 5%/Month) of 3 year and 5 year of amount 161258/-
+
+a=161258
+e3=a/36
+e5=a/60
+
+emi3=e3+(0.05*e3)
+
+emi5=e5+(0.05*e5)
+
+print("\nEMI for 3 years with intrest 5%",emi3)
+
+print("EMI for 5 years with intrest 5%",emi5)
+
+
 
       
 
