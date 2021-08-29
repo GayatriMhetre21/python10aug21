a62ad54f3d77ca5c8d166ba560da33ceb1c7bc8d
Gayatri Mhtre <88704532+GayatriMhetre21@users.noreply.github.com>
2021-08-29 15:17:53 +0530

    Add files via upload

---
+++ arithimaticcall.py
@@ -0,0 +1,11 @@
+from arthimatic.addition.add import add
+from arthimatic.subtraction.sub import sub
+from arthimatic.division.divi import divi
+from arthimatic.modules.mod import mod
+from arthimatic.multiplication.mult import mult
+
+add(2,4)
+sub(2,4)
+divi(2,4)
+mod(2,4)
+mult(2,4)
