c700c5655c1bd92485e56333c552e081d5bbb310
Gayatri Mhtre <88704532+GayatriMhetre21@users.noreply.github.com>
2021-08-29 14:25:34 +0530

    Delete acircle.py

--- Area/circle/acircle.py
+++
@@ -1,8 +0,0 @@
-PI = 3.14
-
-def areaofcircle(radius):
-    try:
-        result = PI * radius * radius
-        print("Area of circle with radius", radius, "is", result, "unit")
-    except OSError as err:
-        print("Radius can't be zero{0}".format(err))
