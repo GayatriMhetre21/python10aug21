af8ae468e83d51628d2110743ffeecade64f5944
Gayatri Mhtre <88704532+GayatriMhetre21@users.noreply.github.com>
2021-08-29 15:35:16 +0530

    Add files via upload

---
+++ Assignment2.que1/Area/circle/acircle.py
@@ -0,0 +1,7 @@
+PI = 3.14
+def areaofcircle(radius):
+    try:
+        result = PI * radius * radius
+        print("Area of circle with radius:", radius, "is", result, "unit")
+    except OSError as err:
+        print("Radius can't be zero{0}".format(err))
