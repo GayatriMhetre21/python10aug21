866c4117f2363cc741862b201bd0d6dd7af0b7bf
Gayatri Mhtre <88704532+GayatriMhetre21@users.noreply.github.com>
2021-09-02 15:10:58 +0530

    Add files via upload

--- 2sep21db/studentdb.py
+++ 2sep21db/studentdb.py
@@ -1,1 +1,20 @@
+#write a program to  create database studentdb
+import MySQLdb
 
+try:
+    query="CREATE DATABASE studentdb "
+    mycon=MySQLdb.connect(host="localhost",user="root",passwd="")
+    print("mycon excecute")
+    cur=mycon.cursor()
+    print("cursor excecute")
+    cur.execute(query)
+    print("execute query")
+    mycon.commit()
+    print(query+"table created .... ")
+except:
+    print("values inserted...")
+finally:
+    cur.close()
+    print("cursor connection close....")
+    mycon.close()
+    print("DB connection")
