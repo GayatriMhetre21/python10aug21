9aacbfbf2c08a96e652e477544e45c9507c5a385
Gayatri Mhtre <88704532+GayatriMhetre21@users.noreply.github.com>
2021-09-10 19:56:23 +0530

    Add files via upload

---
+++ GUI/GU12.py
@@ -0,0 +1,6 @@
+from tkinter import *
+win=Tk()
+win.geometry("200x200+100+200")
+w=Label(win,text="Hello,How are you.....,Welcome to python industrial training.....")
+w.pack()
+win.mainloop()
---
+++ GUI/GUI3.py
@@ -0,0 +1,5 @@
+from tkinter import *
+window=Tk()
+window.title("it's my grafics user interface")
+window.geometry('300x200+10+20')
+window.mainloop()
